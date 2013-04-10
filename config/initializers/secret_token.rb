ed92979fa841c4ccce9d2342471f4e2a484a38e8
James English <james.english@shapehq.co.uk>
2013-04-10 13:50:21 +0100

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nebula::Application.config.secret_token = 'b495320694766f8285d67aa9b3df50647b27a80486e50365f1074aedc37529cca93233a398290d8f88a5ab145c61f17b477225b0f965465e9bcab1055b29d4e8'
